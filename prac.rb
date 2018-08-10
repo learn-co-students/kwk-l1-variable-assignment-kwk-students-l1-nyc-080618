8315842669f39a7e035f5a98005426eb75b7a5ce
sophiaakhter <sophakhter@gmail.com>
2018-08-10 13:12:09 +0000

    Done.

---
+++ prac.rb
@@ -0,0 +1,5 @@
+first_name = "Sophia"
+last_name = "Akhter"
+age = 16
+puts "My name is " + first_name + " " + last_name
+puts "My first name is #{first_name} and my last name is #{last_name} and I am #{age.to_s} years old."
